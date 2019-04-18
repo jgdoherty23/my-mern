4e2e5cb81bd12657598637cf844e0ab20a3159f6
Jackson Doherty <jgdoherty23@gmail.com>
2019-04-18 15:30:13 -0700

    initial commit, importing dependencies and setting up express server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://brad1234:brad1234@mernshoppinglistcluster-g8nzx.mongodb.net/test?retryWrites=true"
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// body-parser middleware
+app.use(bodyParser.json());
+
